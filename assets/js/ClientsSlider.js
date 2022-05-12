3d37f94dd77dcedfa95c97eaecdd148f784e3d8e
Marcos Mendez <mendez.foto@gmail.com>
2022-05-12 10:37:52 -0300

    Update Sergio

---
+++ assets/js/ClientsSlider.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  if ($(".brands_slider").length) {
+    var brandsSlider = $(".brands_slider");
+
+    brandsSlider.owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      nav: false,
+      dots: false,
+      items: 7,
+      margin: 2,
+      responsive: {
+        0: { items: 1 },
+        412: { items: 2 },
+        768: { items: 4 },
+        991: { items: 4 },
+        1199: { items: 7 },
+      },
+    });
+
+    if ($("brandsSlider").width() < 768) {
+      brandsSlider.owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        items: 2,
+        margin: 2,
+      });
+    }
+
+    if ($(".brands_prev").length) {
+      var prev = $(".brands_prev");
+      prev.on("click", function () {
+        brandsSlider.trigger("prev.owl.carousel");
+      });
+    }
+
+    if ($(".brands_next").length) {
+      var next = $(".brands_next");
+      next.on("click", function () {
+        brandsSlider.trigger("next.owl.carousel");
+      });
+    }
+  }
+});
